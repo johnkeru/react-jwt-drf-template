510ba0a5f3ea3d5d1916bae390750f1ec0abc003
john keru <keruk355@gmail.com>
2023-01-31 23:10:53 +0800

    pagination added

--- main/settings.py
+++ main/settings.py
@@ -147,7 +147,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 10
 }
 
 from datetime import timedelta
